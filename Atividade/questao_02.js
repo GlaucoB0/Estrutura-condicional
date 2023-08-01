c5b4c97d81ad4dd2517355cd912651b375eaa320
Glauco2E <oniiichaan61@gmail.com>
2023-08-01 08:31:22 -0300

    atividade 2 feita

---
+++ Atividade/questao_02.js
@@ -0,0 +1,15 @@
+const nota1 = Number(prompt("Nota 1: "))
+const nota2 = Number(prompt("Nota 2: "))
+const nota3 = Number(prompt("Nota 3: "))
+const nota4 = Number(prompt("Nota 4: "))
+
+let media = (nota1 + nota2 + nota4 + nota4)/4
+
+if (media >= 7 ) {
+    alert("aprovado!")
+    
+} else if (media < 7) {
+    alert("reprovado!")
+} else {
+    
+} 
