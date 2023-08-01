30edf97a7c6ff301074b2703bd1a8cd9cd2631fd
Glauco2E <oniiichaan61@gmail.com>
2023-08-01 08:21:41 -0300

    questao 1 da atividade feita

---
+++ Atividade/questao_01.js
@@ -0,0 +1,9 @@
+let numero1 = Number(prompt("digite um numero"))
+let numero2 = Number(prompt("digite um numero"))
+
+if (numero1 > numero2) {
+    alert(`${numero1} é maior que ${numero2}`)
+    
+} else if (numero2 > numero1){
+    alert(`${numero2} é maior que ${numero1}`)
+}
