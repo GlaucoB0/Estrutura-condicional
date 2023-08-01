d9f21fc0580235f08dcaf27fd89c1206a35d101e
Glauco2E <oniiichaan61@gmail.com>
2023-08-01 12:44:56 -0300

    questao 4 finalizada

--- Atividade/questao_04.js
+++ Atividade/questao_04.js
@@ -1,35 +1,32 @@
 let bairro = prompt("Digite seu bairro: ")
 
 switch (bairro) {
-    case "Antares":
-        alert("R$7,00")
+    case "Antares" || "antares":
+        alert("R$7,00");
         break;
 
-    case "Barro Duro":
-        alert("R$5,00")
+    case "Barro Duro" || "barro duro" || "Barro duro":
+        alert("R$5,00");
         break;
 
-    case "Bebedouro":
-        alert("R$8,00")
+    case "Bebedouro" || "bebedouro":
+        alert("R$8,00");
         break;
 
-    case "Benedito Bentes":
+    case "Benedito Bentes" || "Benedito bentes" || "benedito bentes" || "Benedito" || "benedito":
         alert("R$6,00")
         break;
 
-    case "Centro":
-        alert("R$10,00")
+    case "Centro" || "centro":
+        alert("R$10,00");
         break;
 
-    case "Clima Bom":
-        alert("R$4,00")
+    case "Clima Bom" || "clima bom" || "Clima bom":
+        alert("R$4,00");
         break;
     
-    case "Pajuçara", "Ponta Verde", "Jatiúca":
-        alert("R$15,00")
+    case "Pajuçara" || "pajuçara" || "Pajucara" || "pajucara" || "Ponta Verde" || "ponta verde" || "Ponta verde" || "Jatiúca" || "jatiúca" || "Jatiuca" || "jatiuca":
+        alert("R$15,00");
         break;
 
-        default:
-        alert("Digite corretamente")
-        break;
 }
