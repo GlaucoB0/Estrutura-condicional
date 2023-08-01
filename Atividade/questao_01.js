1375a5df7a3f06f244e39426d77a9589583fbd86
Glauco2E <oniiichaan61@gmail.com>
2023-08-01 09:28:29 -0300

    atividade 2 melhorado

--- Atividade/questao_01.js
+++ Atividade/questao_01.js
@@ -3,7 +3,8 @@ let numero2 = Number(prompt("digite um numero"))
 
 if (numero1 > numero2) {
     alert(`${numero1} é maior que ${numero2}`)
-    
+
 } else if (numero2 > numero1){
     alert(`${numero2} é maior que ${numero1}`)
 }
+
--- Atividade/questao_02.js
+++ Atividade/questao_02.js
@@ -1,15 +1,34 @@
 const nota1 = Number(prompt("Nota 1: "))
+if(nota1 > 10 || nota1 < 1){
+    alert("digite um numero de 1 a 10")}
+else if(nota1 >= 1 || nota1 <= 10){
+
 const nota2 = Number(prompt("Nota 2: "))
+if(nota2 > 10 || nota2 < 1){
+    alert("digite um numero de 1 a 10")}
+else if(nota2 >= 1 || nota2 <= 10){
+
 const nota3 = Number(prompt("Nota 3: "))
+if(nota3 > 10 || nota3 < 1){
+    alert("digite um numero de 1 a 10")}
+else if(nota3 >= 1 || nota3 <= 10){
+
+
 const nota4 = Number(prompt("Nota 4: "))
 
-let media = (nota1 + nota2 + nota4 + nota4)/4
+let media = (nota1 + nota2 + nota3 + nota4)/4
 
 if (media >= 7 ) {
     alert("aprovado!")
-    
+
 } else if (media < 7) {
     alert("reprovado!")
 } else {
     
-} 
+}
+}
+}
+}
+
+
+
