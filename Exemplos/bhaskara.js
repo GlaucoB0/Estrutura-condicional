bb48ebdcd3e9167c2cedc141b33811eeea910922
Glauco2E <oniiichaan61@gmail.com>
2023-08-17 12:44:06 -0300

    exemplos feitos

--- Exemplos/bhaskara.js
+++ Exemplos/bhaskara.js
@@ -1,11 +1,12 @@
-let a = 1;
-let b = -3;
-let c = -54;
-let bask_p;
-let bask_n;
-let delta = (b*b)-4*a*c;
+let a = Number(prompt("valor de A:"))
+let b = Number(prompt("valor de B:"))
+let c = Number(prompt("valor de C:"))
+let bask_p
+let bask_n
+let delta = (b*b)*(-4)*a*c
 
-bask_p = ((-b) + Math.sqrt(delta)) /(2*a);
-bask_n = ((-b) - Math.sqrt(delta)) /(2*a);
+bask_p = ((-b) + Math.sqrt(delta)) /(2*a)
+bask_n = ((-b) - Math.sqrt(delta)) /(2*a)
 
-console.log(`x1 = ${bask_p}\nx2 = ${bask_n}`)
+console.log(bask_p)
+
