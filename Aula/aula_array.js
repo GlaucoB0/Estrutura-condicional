8917419b0c2db721b59921a8f408afb083b01d8b
Glauco2E <oniiichaan61@gmail.com>
2023-08-31 08:42:31 -0300

    alguma coisa feita ai

---
+++ Aula/aula_array.js
@@ -0,0 +1,26 @@
+/*let tempAnual = [];
+tempAnual[0] = 31.9;
+tempAnual[1] = 35.3;
+tempAnual[2] = 42.5;
+tempAnual[3] = 25.8;
+tempAnual[4] = 23.6;
+tempAnual[5] = 28.6;*/
+
+/*let diasDaSemana = []
+diasDaSemana[0] = "Domingo" 
+diasDaSemana[1] = "Segunda" 
+diasDaSemana[2] = "Terça" 
+diasDaSemana[3] = "Quarta" 
+diasDaSemana[4] = "Quinta" 
+diasDaSemana[5] = "Sexta" 
+diasDaSemana[6] = "Sábado" 
+
+console.log(diasDaSemana)*/
+
+let num = []
+num[0] = 42
+num[4] = 89
+num[2] = 21
+num[1] = 56
+
+console.log(num[1])
